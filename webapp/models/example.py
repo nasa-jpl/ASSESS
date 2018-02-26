aa54c767efe7681a098261c9db8807bdac64fcde
vlall <lall@berkeley.edu>
2018-02-26 13:32:40 -0800

    Add training objects

--- webapp/models/example.py
+++ webapp/models/example.py
@@ -1,10 +1,15 @@
 from classify import Classify
-from trainer import Trainer
+from trainer import Trainer, Word2Vec, DemoClass
 import pandas as pd
 
 
-def benchmark(text, dir, model):
-    """Benchmark training model against validation set."""
+def build(data, model):
+    """Build a model using training data."""
+    model = DemoClass(data)
+    return model
+
+def transform(text, dir=None):
+    """Given a text, transform to prepare for training."""
     df_ieee = pd.read_csv(os.path.join(dir, text), index_col=0)
     df_ieee = df_ieee.reset_index()
     df_ieee = df_ieee.fillna('')
@@ -20,8 +25,12 @@ def benchmark(text, dir, model):
         texts_all.append((texts_abs[i] + texts_scp[i] + texts_pur[i]+texts_int[i]).decode('utf-8','ignore'))
 
     df_ieee['Introduction']=texts_int
+    return text_all
 
 
-ieee_standards = 'IEEE-standards_rev1.csv'
-standards_dir = 'standards'
-benchmark(text=ieee_standards, dir=standards_dir)
+raw_data = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et 
+magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies 
+nec, pellentesque eu, pretium quis, sem."""
+training_data = transform(raw_data)
+model = build(training_data)
