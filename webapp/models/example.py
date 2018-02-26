cbdf35817fc9f406e7571a0d01f50721a1d27878
vlall <vishal.h.lall@gmail.com>
2018-02-26 13:35:53 -0800

    Add demo class

--- webapp/models/example.py
+++ webapp/models/example.py
@@ -1,12 +1,12 @@
 from classify import Classify
 from trainer import Trainer, Word2Vec, DemoClass
 import pandas as pd
-
+import os
 
 def build(data, model):
     """Build a model using training data."""
     model = DemoClass(data)
-    return model
+    return model.demo_function()
 
 def transform(text, dir=None):
     """Given a text, transform to prepare for training."""
@@ -32,5 +32,6 @@ raw_data = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et 
 magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies 
 nec, pellentesque eu, pretium quis, sem."""
-training_data = transform(raw_data)
-model = build(training_data)
+#training_data = transform(raw_data)
+model = build(raw_data, "demo")
+print model
