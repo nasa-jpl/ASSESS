861ded3bf951614c1fe477f4366a4bc8ca209b75
vlall <lall@berkeley.edu>
2018-03-14 14:43:59 -0700

    Add benchmark for testing

---
+++ webapp/models/benchmark_demo.py
@@ -0,0 +1,185 @@
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from gensim.models.word2vec import Word2Vec
+from collections import Counter, defaultdict
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import StratifiedShuffleSplit
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+
+
+# Using csv instead of ES index for now.
+df = pd.read_csv('../standards/IEEE-standards_rev1.csv', encoding = "ISO-8859-1")
+df.columns = [c.replace(' ', '_') for c in df.columns]
+training = df.abstract_new
+frames =  [df.abstract_new, df.scope_new, df.purpose_new]
+training = pd.concat(frames)
+labels = df.Category
+stop = set(stopwords.words('english'))
+all_stops = stop | set(string.punctuation)
+
+GLOVE_6B_50D_PATH = path + "glove.6B.50d.txt"
+GLOVE_840B_300D_PATH = path + "glove.840B.300d.txt"
+encoding="utf-8"
+
+X, y = [], []
+
+"""This is where you customize your tokenizer and seperate labels and text"""
+for item in df.itertuples():
+    if pd.isnull(item.abstract_new) or pd.isnull(item.Category) or len(item.abstract_new) < 2:
+        continue
+    text = str(item.abstract_new) + str(item.scope_new) + str(item.purpose_new)
+    print(text)
+    words = word_tokenize(text)
+    text_no_stop_words_punct = [t for t in words if t not in stop and t not in string.punctuation]
+    wordList = []
+    for word in text_no_stop_words_punct:
+        wordList.append(word)
+    X.append(wordList)
+    print(item.Category)
+    y.append(item.Category)
+
+    print("*START*")
+    print(wordList)
+    print("============")
+    print(item.Category)
+    print("*END*")
+
+X, y = np.array(X), np.array(y)
+print ("total examples %s" % len(y))
+with open(GLOVE_6B_50D_PATH, "rb") as lines:
+    wvec = {line.split()[0].decode(encoding): np.array(line.split()[1:], dtype=np.float32)
+            for line in lines}
+
+glove_small = {}
+all_words = set(w for words in X for w in words)
+with open(GLOVE_6B_50D_PATH, "rb") as infile:
+    for line in infile:
+        parts = line.split()
+        word = parts[0].decode(encoding)
+        if (word in all_words):
+            nums = np.array(parts[1:], dtype=np.float32)
+            glove_small[word] = nums
+
+glove_big = {}
+with open(GLOVE_840B_300D_PATH, "rb") as infile:
+    for line in infile:
+        parts = line.split()
+        word = parts[0].decode(encoding)
+        if word in all_words:
+            nums = np.array(parts[1:], dtype=np.float32)
+            glove_big[word] = nums
+
+# train word2vec
+model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
+w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.syn0)}
+
+
+mult_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("multinomial nb", MultinomialNB())])
+bern_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("bernoulli nb", BernoulliNB())])
+mult_nb_tfidf = Pipeline(
+    [("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("multinomial nb", MultinomialNB())])
+bern_nb_tfidf = Pipeline([("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("bernoulli nb", BernoulliNB())])
+svc = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
+svc_tfidf = Pipeline(
+    [("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
+
+
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        if len(word2vec) > 0:
+            self.dim = len(word2vec[next(iter(glove_small))])
+        else:
+            self.dim = 0
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+
+
+# and a tf-idf version of the same
+class TfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        if len(word2vec) > 0:
+            self.dim = len(word2vec[next(iter(glove_small))])
+        else:
+            self.dim = 0
+
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        # if a word was never seen - it must be at least as infrequent
+        # as any of the known words - so the default idf is the max of
+        # known idf's
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] * self.word2weight[w]
+                     for w in words if w in self.word2vec] or
+                    [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+
+
+# Extra Trees classifier is like random forrest
+etree_glove_small = Pipeline([("glove vectorizer", MeanEmbeddingVectorizer(glove_small)),
+                              ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+etree_glove_small_tfidf = Pipeline([("glove vectorizer", TfidfEmbeddingVectorizer(glove_small)),
+                                    ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+etree_glove_big = Pipeline([("glove vectorizer", MeanEmbeddingVectorizer(glove_big)),
+                            ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+etree_glove_big_tfidf = Pipeline([("glove vectorizer", TfidfEmbeddingVectorizer(glove_big)),
+                                  ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+
+etree_w2v = Pipeline([("word2vec vectorizer", MeanEmbeddingVectorizer(w2v)),
+                      ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+etree_w2v_tfidf = Pipeline([("word2vec vectorizer", TfidfEmbeddingVectorizer(w2v)),
+                            ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+all_models = [
+    ("mult_nb", mult_nb),
+    ("mult_nb_tfidf", mult_nb_tfidf),
+    ("bern_nb", bern_nb),
+    ("bern_nb_tfidf", bern_nb_tfidf),
+    ("svc", svc),
+    ("svc_tfidf", svc_tfidf),
+    ("w2v", etree_w2v),
+    ("w2v_tfidf", etree_w2v_tfidf),
+    ("glove_small", etree_glove_small),
+    ("glove_small_tfidf", etree_glove_small_tfidf),
+    ("glove_big", etree_glove_big),
+    ("glove_big_tfidf", etree_glove_big_tfidf),
+
+]
+
+unsorted_scores = [(name, cross_val_score(model, X, y, cv=5).mean()) for name, model in all_models]
+scores = sorted(unsorted_scores, key=lambda x: -x[1])
+print(tabulate(scores, floatfmt=".4f", headers=("model", 'score')))
+plt.figure(figsize=(15, 6))
+sns.barplot(x=[name for name, _ in scores], y=[score for _, score in scores])
