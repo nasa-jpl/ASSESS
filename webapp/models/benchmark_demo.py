2997daa35df88e3799bdd04623623d328d7e4eb3
vlall <lall@berkeley.edu>
2018-03-23 11:40:19 -0700

    Add comments

--- webapp/models/benchmark_demo.py
+++ webapp/models/benchmark_demo.py
@@ -25,6 +25,8 @@ import os
 import signal
 import dill as pickle
 
+
+# This is a timeout function we can use later.
 class TimeoutError(Exception):
     pass
 
@@ -43,90 +45,8 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
             return result
 
         return wraps(func)(wrapper)
-
     return decorator
 
-
-# Using csv instead of ES index for now.
-path = "/Users/vishall/prog/extras/delete/"
-df = pd.read_json("../../iso_flat.json")
-# Convert to Tuples
-df.ics = df.ics.transform(lambda x: tuple(x))
-df.sections = df.sections.transform(lambda x: x[1:])
-# In order to run this you need Tuples.
-df = df[df.groupby('ics').sections.transform(len) > 10]
-labels = df.ics
-stop = set(stopwords.words('english'))
-all_stops = stop | set(string.punctuation)
-
-GLOVE_6B_50D_PATH = path + "glove.6B.50d.txt"
-GLOVE_840B_300D_PATH = path + "glove.840B.300d.txt"
-encoding="utf-8"
-
-X, y = [], []
-
-for item in df.itertuples():
-    """This is where you customize your tokenizer and seperate labels and text,
-    defining your Xs and Ys"""
-    if not item.ics or not item.sections or len(item.sections) < 1 or len(' '.join(item.sections)) < 10:
-        continue
-    text = item.sections
-    # Include the title. check how many standards have scope.
-    text = item.description + ' ' + ' '.join(text)
-    words = word_tokenize(text)
-    text_no_stop_words_punct = [t for t in words if t not in stop and t not in string.punctuation]
-    wordList = []
-    for word in text_no_stop_words_punct:
-        wordList.append(word)
-    X.append(wordList)
-    field = item.ics[0].split('.')[0]
-    print(field)
-    y.append(field)
-
-    print("### START: Input text ###")
-    print(wordList)
-    print("========================")
-    print(field)
-    print("### END ###")
-
-X, y = np.array(X), np.array(y)
-print ("total examples %s" % len(y))
-with open(GLOVE_6B_50D_PATH, "rb") as lines:
-    wvec = {line.split()[0].decode(encoding): np.array(line.split()[1:], dtype=np.float32)
-            for line in lines}
-
-glove_small = {}
-all_words = set(w for words in X for w in words)
-with open(GLOVE_6B_50D_PATH, "rb") as infile:
-    for line in infile:
-        parts = line.split()
-        word = parts[0].decode(encoding)
-        if (word in all_words):
-            nums = np.array(parts[1:], dtype=np.float32)
-            glove_small[word] = nums
-
-glove_big = {}
-with open(GLOVE_840B_300D_PATH, "rb") as infile:
-    for line in infile:
-        parts = line.split()
-        word = parts[0].decode(encoding)
-        if word in all_words:
-            nums = np.array(parts[1:], dtype=np.float32)
-            glove_big[word] = nums
-
-# train word2vec
-model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
-w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.syn0)}
-
-
-mult_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("multinomial nb", MultinomialNB())])
-bern_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("bernoulli nb", BernoulliNB())])
-mult_nb_tfidf = Pipeline(
-    [("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("multinomial nb", MultinomialNB())])
-bern_nb_tfidf = Pipeline([("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("bernoulli nb", BernoulliNB())])
-svc = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
-svc_tfidf = Pipeline(
-    [("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
 
 
 class MeanEmbeddingVectorizer(object):
@@ -179,7 +99,91 @@ class TfidfEmbeddingVectorizer(object):
             for words in X
         ])
 
+"""
+TODO:
+Clean up this section into it's own pre-processing function.
+"""
+# Using csv instead of ES index for now.
+path = "/Users/vishall/prog/extras/delete/"
+df = pd.read_json("../../iso_flat.json")
+# Convert to tuples in order to use the groupby function.
+df.ics = df.ics.transform(lambda x: tuple(x))
+df.sections = df.sections.transform(lambda x: x[1:])
+# In order to run this you need Tuples.
+df = df[df.groupby('ics').sections.transform(len) > 10]
+labels = df.ics
+stop = set(stopwords.words('english'))
+all_stops = stop | set(string.punctuation)
 
+GLOVE_6B_50D_PATH = path + "glove.6B.50d.txt"
+GLOVE_840B_300D_PATH = path + "glove.840B.300d.txt"
+encoding="utf-8"
+
+X, y = [], []
+
+for item in df.itertuples():
+    """This is where you customize your tokenizer and seprate labels and text,
+    defining your Xs and Ys"""
+    if not item.ics or not item.sections or len(item.sections) < 1 or len(' '.join(item.sections)) < 10:
+        continue
+    text = item.sections
+    # Include the title. check how many standards have scope.
+    text = item.description + ' ' + ' '.join(text)
+    words = word_tokenize(text)
+    text_no_stop_words_punct = [t for t in words if t not in stop and t not in string.punctuation]
+    wordList = []
+    for word in text_no_stop_words_punct:
+        wordList.append(word)
+    X.append(wordList)
+    field = item.ics[0].split('.')[0]
+    print(field)
+    y.append(field)
+
+    print("### START: Input text ###")
+    print(wordList)
+    print("========================")
+    print(field)
+    print("### END ###")
+
+X, y = np.array(X), np.array(y)
+print ("total examples %s" % len(y))
+with open(GLOVE_6B_50D_PATH, "rb") as lines:
+    wvec = {line.split()[0].decode(encoding): np.array(line.split()[1:], dtype=np.float32)
+            for line in lines}
+
+glove_small = {}
+all_words = set(w for words in X for w in words)
+with open(GLOVE_6B_50D_PATH, "rb") as infile:
+    for line in infile:
+        parts = line.split()
+        word = parts[0].decode(encoding)
+        if (word in all_words):
+            nums = np.array(parts[1:], dtype=np.float32)
+            glove_small[word] = nums
+
+glove_big = {}
+with open(GLOVE_840B_300D_PATH, "rb") as infile:
+    for line in infile:
+        parts = line.split()
+        word = parts[0].decode(encoding)
+        if word in all_words:
+            nums = np.array(parts[1:], dtype=np.float32)
+            glove_big[word] = nums
+
+# Train word2vec
+model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
+w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.syn0)}
+
+
+
+mult_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("multinomial nb", MultinomialNB())])
+bern_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("bernoulli nb", BernoulliNB())])
+mult_nb_tfidf = Pipeline(
+    [("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("multinomial nb", MultinomialNB())])
+bern_nb_tfidf = Pipeline([("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("bernoulli nb", BernoulliNB())])
+svc = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
+svc_tfidf = Pipeline(
+    [("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
 # Extra Trees classifier is like random forrest
 etree_glove_small = Pipeline([("glove vectorizer", MeanEmbeddingVectorizer(glove_small)),
                               ("extra trees", ExtraTreesClassifier(n_estimators=200))])
