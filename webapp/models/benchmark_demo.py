6b04ee75ee5f1d7fbd15ec62a3fe3c85cdd3f34a
vlall <lall@berkeley.edu>
2018-04-12 09:24:39 -0700

    Refactor code

--- webapp/models/benchmark_demo.py
+++ webapp/models/benchmark_demo.py
@@ -24,55 +24,7 @@ import errno
 import os
 import signal
 import dill as pickle
-
-
-# We can use later for decorating functions with a timeout.
-
-class MeanEmbedVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        if len(word2vec) > 0:
-            self.dim = len(word2vec[next(iter(glove_small))])
-        else:
-            self.dim = 0
-
-    def fit(self, X, y):
-        return self
-
-    def transform(self, X):
-        return np.array([
-            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
-                    or [np.zeros(self.dim)], axis=0)
-            for words in X
-        ])
-
-
-class TfidfEmbedVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        self.word2weight = None
-        if len(word2vec) > 0:
-            self.dim = len(word2vec[next(iter(glove_small))])
-        else:
-            self.dim = 0
-
-    def fit(self, X, y):
-        tfidf = TfidfVectorizer(analyzer=lambda x: x)
-        tfidf.fit(X)
-        max_idf = max(tfidf.idf_)
-        self.word2weight = defaultdict(
-            lambda: max_idf,
-            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
-
-        return self
-
-    def transform(self, X):
-        return np.array([
-            np.mean([self.word2vec[w] * self.word2weight[w]
-                     for w in words if w in self.word2vec] or
-                    [np.zeros(self.dim)], axis=0)
-            for words in X
-        ])
+from vectorizer import MeanEmbedVectorizer, TfidfVectorizer
 
 """
 TODO:
