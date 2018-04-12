52dc97a9d924c9e08c65deac44dc11208a7a8fc1
vlall <lall@berkeley.edu>
2018-04-12 09:22:31 -0700

    Remove Timeout

--- webapp/models/benchmark_demo.py
+++ webapp/models/benchmark_demo.py
@@ -27,26 +27,6 @@ import dill as pickle
 
 
 # We can use later for decorating functions with a timeout.
-class TimeoutError(Exception):
-    pass
-
-def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
-    def decorator(func):
-        def _handle_timeout(signum, frame):
-            raise TimeoutError(error_message)
-
-        def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handle_timeout)
-            signal.alarm(seconds)
-            try:
-                result = func(*args, **kwargs)
-            finally:
-                signal.alarm(0)
-            return result
-
-        return wraps(func)(wrapper)
-    return decorator
-
 
 class MeanEmbedVectorizer(object):
     def __init__(self, word2vec):
