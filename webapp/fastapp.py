3d95d32afa7d2c21673598158ae73308475fd834
vlall <lall@berkeley.edu>
2020-07-22 18:00:23 -0700

    Add FASTAPI port

---
+++ webapp/fastapp.py
@@ -0,0 +1,133 @@
+from flask import Flask, send_from_directory, safe_join
+import os
+import json
+import subprocess
+from flask import request
+from flask_cors import CORS, cross_origin
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import normalize
+import dill
+import pandas as pd
+from sklearn.feature_extraction import text
+from standard_extractor import find_standard_ref
+#from text_analysis.utils import loadmodel
+from text_analysis import extract_prep
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, ORJSONResponse, JSONResponse
+from fastapi import FastAPI, File, Form, UploadFile, Request
+from fastapi.encoders import jsonable_encoder
+import requests
+
+app = FastAPI()
+#app.static_folder = "webui"
+origins = [
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+"""BEGIN
+@app.get('/')
+def index():    #return send_from_directory('webui/', 'index.html')
+    webapi = FastAPI(openapi_url="index.html")
+    webapi.mount("/webui", StaticFiles(directory="webui"))
+    app.mount("/static", StaticFiles(directory="webui/"), name="webui")
+    #return HTMLResponse(pkg_resources.resource_string(__name__, 'static/index.html'))
+    return HTMLResponse(content=html_content, status_code=200)
+END"""
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    host = request.url
+    recc_text_url = str(host) +'recommend_text'
+    recc_file_url = str(host) +'recommend_file'
+    extract_url = str(host) + 'extract'
+    st_info_url = str(host) + 'standard_info'
+    print(recc_text_url)
+    content = """
+    <html>
+        <head>
+            <title>ASSESS API</title>
+        </head>
+        <body>
+            <h1>Welcome to the ASSESS API </h1>
+            <h2>API Endpoints</h2>
+            <p>{recc_text_url}</p>
+            <p>{recc_file_url}</p>
+            <p>{extract_url}</p>
+            <p>{st_info_url}</p>
+        </body>
+    </html>""".format(recc_text_url=recc_text_url, recc_file_url=recc_file_url,
+     extract_url=extract_url, st_info_url=st_info_url)
+    return HTMLResponse(content=content, status_code=200)
+
+
+@app.get('/recommend_text/(text)')
+async def recommend_text(text: str):
+    """
+    GET from text
+    """
+    prediction = extract_prep.predict(text=text)
+    json_compatible_item_data = jsonable_encoder(prediction)
+    return JSONResponse(content=json_compatible_item_data)
+
+@app.post('/recommend_file')
+async def recommend_file(pdf: bytes = File(...)):
+    """
+    POST from PDF
+    """
+    prediction = extract_prep.predict(files=pdf)
+    json_compatible_item_data = jsonable_encoder(prediction)
+    return JSONResponse(content=json_compatible_item_data)
+
+
+@app.post('/extract')
+async def extract(pdf: bytes = File(...)):
+    """
+    POST from PDF
+    """
+    refs = extract_prep.extract_standard_ref(filename=pdf)
+    json_compatible_item_data = jsonable_encoder(refs)
+    return JSONResponse(content=json_compatible_item_data)
+
+
+@app.get('/standard_info/(pdf_location)', response_class=ORJSONResponse)
+async def standard_info(pdf_location: str):
+    """
+    GET standard info given a unique standard identifier
+    """
+    standard_details = [{"id": pdf_location}]
+    json_compatible_item_data = jsonable_encoder(standard_details)
+    return JSONResponse(content=json_compatible_item_data)
+
+
+# Incomplete
+@app.get('/save_activity', response_class=HTMLResponse)
+async def save_activity():
+    return
+
+
+# Incomplete
+@app.get('/search', response_class=HTMLResponse)
+async def search():
+    return
+
+
+# Incomplete
+@app.get('/add_standards', response_class=HTMLResponse)
+async def add_standards():
+    return
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
---
+++ webapp/text_analysis/extract_prep.py
@@ -0,0 +1,144 @@
+
+from flask import Flask, send_from_directory, safe_join
+import os
+import json
+import subprocess
+from flask import request
+from flask_cors import CORS, cross_origin
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import normalize
+import dill
+import pandas as pd
+from sklearn.feature_extraction import text
+from standard_extractor import find_standard_ref
+import pathlib
+
+
+def load_data():
+    dirPath = str(pathlib.Path(__file__).parent.absolute())
+
+    standards_dir = dirPath + '/../standards/data'
+    json_output_dir = 'output'
+    models_dir='models'
+
+    df=pd.read_csv(os.path.join(standards_dir,'iso_final_all_clean_text.csv'),index_col=0)
+    df=df[df['type']=='standard'].reset_index(drop=True)
+    df.fillna('', inplace=True)
+
+    tfidftransformer=TfidfVectorizer(ngram_range=(1,1), stop_words=text.ENGLISH_STOP_WORDS)
+    X=tfidftransformer.fit_transform([m+' '+n for m, n in zip(df['description_clean'], df['title'])]) # using both desc and tile to predict
+    # tfidftransformer=TfidfVectorizer(ngram_range=(1,1))
+    # X=tfidftransformer.fit_transform([m+' '+n for m, n in zip(df['description'], df['title'])]) # using both desc and tile to predict
+    print('shape', X.shape)
+    X=normalize(X, norm='l2', axis=1)
+    nbrs_brute = NearestNeighbors(n_neighbors=X.shape[0], algorithm='brute', metric='cosine')
+    print('fitting')
+    nbrs_brute.fit(X.todense())
+    print('fitted')
+
+    return df
+
+def test():
+    print("hello")
+
+def extract_standard_ref(filename):
+    bashCommand = "java -cp standards_extraction/lib/tika-app-1.16.jar:standards_extraction/bin StandardsExtractor " + \
+                  filename + " 0.75 > " + filename + ".json"
+    output = subprocess.check_output(['bash', '-c', bashCommand])
+    js = json.load(open(filename + '.json'))
+    standard_refs=[]
+    if 'standard_references' in js.keys():
+        standard_refs = js['standard_references']
+    print('no standard refs found!')
+    return standard_refs
+
+
+def parse_text(filepath):
+
+    if os.path.exists(filepath+'_parsed.txt'):
+        # todo: remove this. Caches the parsed text.
+        return str(open(filepath+'_parsed.txt', 'r').read())
+
+    bashCommand = "java -jar standards_extraction/lib/tika-app-1.16.jar -t " + filepath
+    output=''
+    try:
+        output = subprocess.check_output(['bash', '-c', bashCommand])
+        file=open(filepath + '_parsed.txt', 'wb')
+        file.write(output)
+        file.close()
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+    return str(output)
+
+
+#@app.route('/predict',methods=['POST'])
+def predict(files=None, text=None):
+    df = load_data()
+    
+    # How do we get request.file
+    text=''
+    # ======================== find the referenced standards
+    filename='temp_text'
+    # check if the post request has the file part
+    if 'file' in request.files:
+        file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+        if file.filename == '':
+            return 'no selected file!'
+            # return redirect(request.url)
+        if file :
+            filename=file.filename
+            file.save(filename)
+            text=parse_text(filename)
+            print('parsed')
+    else:
+        # get text from form
+        file = open(filename, 'w')
+        file.write(str(text.encode('utf-8', 'ignore')))
+        file.flush()
+        file.close()
+
+    print('extracting standards')
+    # standard_refs=extract_standard_ref(filename)
+    standard_refs=find_standard_ref(text)
+    print('standards extracted')
+
+    # ======================== find the recommended standards
+
+    result={}
+    result['refs'] = standard_refs
+    result['recc'] = []
+
+    sow = tfidftransformer.transform([text])
+    sow = normalize(sow, norm='l2', axis=1)
+
+    print('scoring standards')
+    distances, indices = nbrs_brute.kneighbors(sow.todense())
+    distances = list(distances[0])
+    indices = list(indices[0])
+
+    for indx, dist in zip(indices[:10],distances[:10]):
+        title=df.iloc[indx]['title']
+        description=df.iloc[indx]['description']
+        link=df.iloc[indx]['link']
+        standard_code = df.iloc[indx]['standard']
+
+        # todo: this code calculates the word importances for the top results (slows the operation, hence commented)
+        # print(title)
+        # print(description)
+        #
+        # to_print = [
+        #     tfidftransformer.get_feature_names()[i]
+        #     + ' ' +
+        #     str(abs(np.array(sow[0].todense()).flatten()[i] - np.array(X[indx].todense()).flatten()[i]))
+        #     for i in set(sow.indices).intersection(X[indx].indices)]
+        # print(' || '.join(to_print), '\n')
+
+
+        result['recc'].append(
+            {'title': title+' ('+standard_code.replace('~','')+')', 'description':description, 'url': link, 'sim': 100 * round(1-dist, 2)})
+
+    print('scored standards')
+    return json.dumps(result)
