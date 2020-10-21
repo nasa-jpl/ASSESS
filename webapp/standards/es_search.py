3a2f1dc6435f76bcb40966e5eaca25366cafbae1
vlall <lall@berkeley.edu>
2020-10-21 01:17:01 -0700

    Add search

--- webapp/standards/es_search.py
+++ webapp/standards/es_search.py
@@ -1,13 +1,14 @@
 from elasticsearch import Elasticsearch 
 import json
-from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 import requests
 
 
 es = Elasticsearch(["172.19.0.2"])
 es_index = "iso_final_clean"
+search = Search(using=es)
 
-def search(uri, term):
+def search_test(uri, term):
     """Simple Elasticsearch Query"""
     query = json.dumps({
         "query": {
@@ -21,15 +22,12 @@ def search(uri, term):
     return results
 
 
-def client_search(searchq):
-    res = es.search(index=es_index, body={"query": {"match": {"content": searchq}}})
-    print(res)
-    print("%d documents found" % res['hits']['total'])
-    for doc in res['hits']['hits']:
-        print(doc['_source']['content'])
+def client_search(searchq, n):
+    return es.search(index=es_index, body=searchq, size=n)
 
-def search_by_text(index, searchq):
-    res = es.search(index=index, body={"query": {"match": {"description":searchq}}})
+
+def search_by_text(searchq):
+    res = es.search(index=es_index, body={"query": {"match": {"description":searchq}}})
     print("Got %d Hits:" % res['hits']['total']['value'])
     results = {}
     for num, hit in enumerate(res['hits']['hits']):
@@ -37,8 +35,8 @@ def search_by_text(index, searchq):
     json_object = json.dumps(results, indent=4)
     return json_object
 
-def search_by_id(index, searchq):
-    res = es.search(index=index, body={"query": {"match": {"num_id":searchq}}})
+def search_by_id(searchq):
+    res = es.search(index=es_index, body={"query": {"match": {"num_id":searchq}}})
     print("Got %d Hits:" % res['hits']['total']['value'])
     results = {}
     for num, hit in enumerate(res['hits']['hits']):
@@ -50,4 +48,4 @@ def search_by_id(index, searchq):
 #client_search('localhost:9200/test-csv', 'airplanes')
 print(search_by_text("machine"))
 print(search_by_id("22"))
-
+print(client_search("test", 3))
