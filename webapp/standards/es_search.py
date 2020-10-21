0d773ac96b939e2aee504343575683325e2df152
vlall <lall@berkeley.edu>
2020-10-21 00:44:08 -0700

    Add endpoint changes

--- webapp/standards/es_search.py
+++ webapp/standards/es_search.py
@@ -4,7 +4,8 @@ from elasticsearch import Elasticsearch
 import requests
 
 
-es = Elasticsearch()
+es = Elasticsearch(["172.19.0.2"])
+es_index = "iso_final_clean"
 
 def search(uri, term):
     """Simple Elasticsearch Query"""
@@ -20,8 +21,8 @@ def search(uri, term):
     return results
 
 
-def client_search(index, searchq):
-    res = es.search(index=index, body={"query": {"match": {"content": searchq}}})
+def client_search(searchq):
+    res = es.search(index=es_index, body={"query": {"match": {"content": searchq}}})
     print(res)
     print("%d documents found" % res['hits']['total'])
     for doc in res['hits']['hits']:
@@ -30,19 +31,23 @@ def client_search(index, searchq):
 def search_by_text(index, searchq):
     res = es.search(index=index, body={"query": {"match": {"description":searchq}}})
     print("Got %d Hits:" % res['hits']['total']['value'])
-    for hit in res['hits']['hits']:
-        return(hit["_source"])
-    #print(len(res["hits"]["hits"]))
+    results = {}
+    for num, hit in enumerate(res['hits']['hits']):
+        results[num+1] = hit["_source"]
+    json_object = json.dumps(results, indent=4)
+    return json_object
 
 def search_by_id(index, searchq):
     res = es.search(index=index, body={"query": {"match": {"num_id":searchq}}})
     print("Got %d Hits:" % res['hits']['total']['value'])
-    for hit in res['hits']['hits']:
-        return(hit["_source"])
-    #print(len(res["hits"]["hits"]))
+    results = {}
+    for num, hit in enumerate(res['hits']['hits']):
+        results[num+1] = hit["_source"]
+    json_object = json.dumps(results, indent=4)
+    return json_object
 
 
 #client_search('localhost:9200/test-csv', 'airplanes')
-print(search_by_text("test-csv", "machine"))
-print(search_by_id("test-csv", "22"))
+print(search_by_text("machine"))
+print(search_by_id("22"))
 
