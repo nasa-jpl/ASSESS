4db57cc33a0851daf4326a70a3d46b8e9c75ff52
ericjunkins <eric.junkins@colorado.edu>
2020-11-19 10:50:41 -0700

    Removing references to redux

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import store from "./redux/store"
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
 
-ReactDOM.render(    
-    <Provider store={store}> 
-        <App />
-    </Provider>,
+ReactDOM.render(
+        <App />,
     document.getElementById('root')
 );
 
