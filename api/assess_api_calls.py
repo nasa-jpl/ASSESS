2e44abf13f4e498b28630fec3089b65e2b67d992
Vishal Lall <vishal.h.lall@gmail.com>
2021-03-05 19:24:29 -0500

    Reformat

--- api/assess_api_calls.py
+++ api/assess_api_calls.py
@@ -4,7 +4,7 @@ import time
 
 # Define endpoints
 root = "https://assess-api.jpl.nasa.gov/"
-#root = "http://0.0.0.0:8080/"
+# root = "http://0.0.0.0:8080/"
 urlRec = root + "recommend_text"
 urlRecFile = root + "recommend_file"
 urlExtract = root + "extract"
@@ -14,28 +14,28 @@ urlStandardInfo = root + "standard_info"
 urlSelect = root + "select_standards"
 urlSet = root + "set_standards"
 # Specify file location of an SOW.
-files = {'pdf': open('/Users/user/prog/assess-root/test.pdf', 'rb')}
+# files = {'pdf': open('/Users/user/prog/assess-root/test.pdf', 'rb')}
 
-## Get request to root endpoint.
-print("Testing root.")
-r = requests.get(root)
-print(r.text)
+# ## Get request to root endpoint.
+# print("Testing root.")
+# r = requests.get(root)
+# print(r.text)
 
-# Recommend SoW given string "This is for airplanes".
-print("Testing recommend_text.")
-jsonLoad = {"text_field": "This is for airplanes"}
-r = requests.post(urlRec, json=jsonLoad)
-print(r.text)
+# # Recommend SoW given string "This is for airplanes".
+# print("Testing recommend_text.")
+# jsonLoad = {"text_field": "This is for airplanes"}
+# r = requests.post(urlRec, json=jsonLoad)
+# print(r.text)
 
-# Recommend an SoW given a PDF.
-print("Testing recommend_file.")
-r = requests.post(urlRecFile, files=files)
-print(r.text)
+# # Recommend an SoW given a PDF.
+# print("Testing recommend_file.")
+# r = requests.post(urlRecFile, files=files)
+# print(r.text)
 
-# Extract standard reference from PDF.
-print("Testing extract.")
-r = requests.post(urlExtract, files=files)
-print(r.text)
+# # Extract standard reference from PDF.
+# print("Testing extract.")
+# r = requests.post(urlExtract, files=files)
+# print(r.text)
 
 # Get standard references.
 print("Testing search.")
@@ -44,34 +44,34 @@ print(r.text)
 
 ## Add/Ingest Standard.
 doc = {
-	"num_id": "111111",
-	"code": "test-delete-later",
-	"field": "test-delete-later",
-	"group": "~1.0",
-	"id": "~1.1.1",
-	"id_": "~6",
-	"link": "test",
-	"new_field": "~6",
-	"new_group": "~6",
-	"new_standard": "1",
-	"new_subgroup": "1",
-	"standard": "",
-	"subgroup": "1",
-	"title": "This is an Example Doc",
-	"type": "test_type",
-	"current_status": "Awaiting_Removal",
-	"datetime": "",
-	"description": "Testing index ingestion!",
-	"edition": "1",
-	"ics": "1.1.1",
-	"number_of_pages": "1",
-	"preview_url": "https://example.com",
-	"publication_date": "",
-	"section_titles": "Test Title",
-	"sections": "",
-	"tc": "",
-	"url": "https://example.com",
-	"description_clean": "Testing index ingestion!"
+    "num_id": "111111",
+    "code": "test-delete-later",
+    "field": "test-delete-later",
+    "group": "~1.0",
+    "id": "~1.1.1",
+    "id_": "~6",
+    "link": "test",
+    "new_field": "~6",
+    "new_group": "~6",
+    "new_standard": "1",
+    "new_subgroup": "1",
+    "standard": "",
+    "subgroup": "1",
+    "title": "This is an Example Doc",
+    "type": "test_type",
+    "current_status": "Awaiting_Removal",
+    "datetime": "",
+    "description": "Testing index ingestion!",
+    "edition": "1",
+    "ics": "1.1.1",
+    "number_of_pages": "1",
+    "preview_url": "https://example.com",
+    "publication_date": "",
+    "section_titles": "Test Title",
+    "sections": "",
+    "tc": "",
+    "url": "https://example.com",
+    "description_clean": "Testing index ingestion!",
 }
 print("Testing add_standards")
 r = requests.put(urlAdd, json=doc)
@@ -85,18 +85,18 @@ print(r.text)
 
 # Insert the standard selected by the user into Elasticsearch indices for stats and logs.
 selected = {
-		"username" : "test_user",
-		"standard_key" : [1,2,3],
+    "username": "test_user",
+    "standard_key": [1, 2, 3],
 }
 print("Testing user selected standard.")
 r = requests.post(urlSelect, json=selected)
 print(r.text)
 
 # Insert the standard selected by the admin into Elasticsearch.
-set_standards = { 
-"username" : "test_user",
-"standard_key": 111111,
-"priority" : 100,
+set_standards = {
+    "username": "test_user",
+    "standard_key": 111111,
+    "priority": 100,
 }
 print("Testing Admin set standard.")
 r = requests.put(urlSet, json=set_standards)
--- api/fastapp.py
+++ api/fastapp.py
@@ -45,8 +45,8 @@ app.add_middleware(
 # Connect to Elasticsearch
 es, idx_main, idx_log, idx_stats = connect_to_es()
 
-if not os.path.exists('log'):
-    os.makedirs('log')
+if not os.path.exists("log"):
+    os.makedirs("log")
 
 app.add_middleware(
     CORSMiddleware,
@@ -58,19 +58,16 @@ app.add_middleware(
 
 
 class Sow(BaseModel):
-    text_field: str = Field(
-        example="Airplanes are complex."
-    )
+    text_field: str = Field(example="Airplanes are complex.")
 
 
 def log_stats(request, data=None, user=None):
-    """Log detailed data in JSON for incoming/outgoing API request.
-    """
+    """Log detailed data in JSON for incoming/outgoing API request."""
     client_host = request.client.host
     msg = {}
     # TODO: Log user once authentication is connected.
-    #msg["user"] = str(user)
-    #request.state.time_started = time.time()
+    # msg["user"] = str(user)
+    # request.state.time_started = time.time()
     msg["time_started"] = str(time.time())
     msg["method"] = str(request.method)
     msg["url"] = str(request.url)
@@ -84,15 +81,15 @@ def log_stats(request, data=None, user=None):
     return
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     host = request.url
-    recc_text_url = str(host) +'recommend_text'
-    recc_file_url = str(host) +'recommend_file'
-    extract_url = str(host) + 'extract'
-    st_info_url = str(host) + 'standard_info'
-    search_url = str(host) + 'search'
-    add_url = str(host) + 'add_standards'
+    recc_text_url = str(host) + "recommend_text"
+    recc_file_url = str(host) + "recommend_file"
+    extract_url = str(host) + "extract"
+    st_info_url = str(host) + "standard_info"
+    search_url = str(host) + "search"
+    add_url = str(host) + "add_standards"
     content = """
     <html>
         <head>
@@ -114,18 +111,20 @@ async def index(request: Request):
             <p>https://assess-api.jpl.nasa.gov/redoc</p>
             <p>https://assess-api.jpl.nasa.gov/docs</p>
         </body>
-    </html>""".format(recc_text_url=recc_text_url, recc_file_url=recc_file_url,
-     extract_url=extract_url, st_info_url=st_info_url, search_url=search_url, add_url=add_url)
+    </html>""".format(
+        recc_text_url=recc_text_url,
+        recc_file_url=recc_file_url,
+        extract_url=extract_url,
+        st_info_url=st_info_url,
+        search_url=search_url,
+        add_url=add_url,
+    )
     return HTMLResponse(content=content, status_code=200)
 
 
-@app.post('/recommend_text')
-async def recommend_text(
-    request: Request, 
-    sow: Sow,
-    ):
-    """Recommend standards from input text.
-    """
+@app.post("/recommend_text")
+async def recommend_text(request: Request, sow: Sow):
+    """Recommend standards from input text."""
     in_text = sow.text_field
     prediction = extract_prep.predict(in_text=in_text)
     json_compatible_item_data = jsonable_encoder(prediction)
@@ -133,10 +132,9 @@ async def recommend_text(
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.post('/recommend_file')
+@app.post("/recommend_file")
 async def recommend_file(request: Request, pdf: UploadFile = File(...)):
-    """Recommend standards from PDF.
-    """
+    """Recommend standards from PDF."""
     print("File received")
     prediction = extract_prep.predict(file=pdf)
     log_stats(request, data=pdf.filename)
@@ -144,11 +142,10 @@ async def recommend_file(request: Request, pdf: UploadFile = File(...)):
     return JSONResponse(content=prediction)
 
 
-@app.post('/extract')
+@app.post("/extract")
 async def extract(request: Request, pdf: UploadFile = File(...)):
-    """Extract standards from PDF.
-    """
-    # filepath = save_upload_file_tmp(pdf) 
+    """Extract standards from PDF."""
+    # filepath = save_upload_file_tmp(pdf)
     text = extract_prep.parse_text(pdf.filename)
     refs = find_standard_ref(text)
     json_compatible_item_data = jsonable_encoder(refs)
@@ -156,42 +153,42 @@ async def extract(request: Request, pdf: UploadFile = File(...)):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/standard_info/{info_key}', response_class=ORJSONResponse)
-async def standard_info(
-    request: Request, 
-    info_key: str,
-    size: int = 1
-    ):
-    """GET standard info given a primary key.
-    """
-    res = es.search(index=idx_main, body={"size": size, "query": {"match": {"num_id": info_key}}})
-    #print("Got %d Hits:" % res['hits']['total']['value'])
+@app.get("/standard_info/{info_key}", response_class=ORJSONResponse)
+async def standard_info(request: Request, info_key: str, size: int = 1):
+    """GET standard info given a primary key."""
+    res = es.search(
+        index=idx_main, body={"size": size, "query": {"match": {"num_id": info_key}}}
+    )
+    # print("Got %d Hits:" % res['hits']['total']['value'])
     results = {}
-    for num, hit in enumerate(res['hits']['hits']):
-        results[str(num+1)] = hit["_source"]
-    jsonResults = json.dumps(results, indent = 4)
+    for num, hit in enumerate(res["hits"]["hits"]):
+        results[str(num + 1)] = hit["_source"]
+    jsonResults = json.dumps(results, indent=4)
     json_compatible_item_data = jsonable_encoder(jsonResults)
     log_stats(request, data=info_key)
-    return JSONResponse(content=json_compatible_item_data)    
+    return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/search/{searchq}')
-async def search(request: Request, searchq: str = Field(example="Airplanes"), size: int = 10):
-    """Search elasticsearch.
-    """
-    res = es.search(index=idx_main, body={"size": size, "query": {"match": {"description": searchq}}})
-    #print("Got %d Hits:" % res['hits']['total']['value'])
+@app.get("/search/{searchq}")
+async def search(
+    request: Request, searchq: str = Field(example="Airplanes"), size: int = 10
+):
+    """Search elasticsearch."""
+    res = es.search(
+        index=idx_main,
+        body={"size": size, "query": {"match": {"description": searchq}}},
+    )
+    # print("Got %d Hits:" % res['hits']['total']['value'])
     results = {}
-    for num, hit in enumerate(res['hits']['hits']):
-        results[str(num+1)] = hit["_source"]#["num_id"]
+    for num, hit in enumerate(res["hits"]["hits"]):
+        results[str(num + 1)] = hit["_source"]  # ["num_id"]
     log_stats(request, data=searchq)
-    return JSONResponse(content=results)    
-    
+    return JSONResponse(content=results)
 
-@app.put('/add_standards', response_class=HTMLResponse)
+
+@app.put("/add_standards", response_class=HTMLResponse)
 async def add_standards(request: Request, doc: dict):
-    """Add standards to main ES index.
-    """
+    """Add standards to main ES index."""
     res = es.index(index=idx_main, body=json.dumps(doc))
     print(res)
     json_compatible_item_data = jsonable_encoder(doc)
@@ -199,18 +196,17 @@ async def add_standards(request: Request, doc: dict):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.post('/select_standards')
+@app.post("/select_standards")
 async def select_standards(request: Request, selected: dict):
-    """Capture selected standards.
-    """
+    """Capture selected standards."""
     schema = {
-        "type" : "object",
-        "properties" : {
-            "username" : {"type" : "string"},
-            "standard_key" : {"type" : "array"},
+        "type": "object",
+        "properties": {
+            "username": {"type": "string"},
+            "standard_key": {"type": "array"},
         },
     }
-    #validate(instance={"username" : "user123", "selected" : [1, 2 ,3]}, schema=schema)
+    # validate(instance={"username" : "user123", "selected" : [1, 2 ,3]}, schema=schema)
     validate(instance=selected, schema=schema)
     json_compatible_item_data = jsonable_encoder(selected)
     res = es.index(index=idx_stats, body=json.dumps(selected))
@@ -219,33 +215,33 @@ async def select_standards(request: Request, selected: dict):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.put('/set_standards')
+@app.put("/set_standards")
 async def set_standards(request: Request, set_standards: dict):
-    """Validate and set preference of standards (done by Admin).
-    """
+    """Validate and set preference of standards (done by Admin)."""
     # TODO: Once we are connected to LDAP, add line to verify auth of Admin.
     schema = {
-        "type" : "object",
-        "properties" : {
-            "username" : {"type" : "string"},
-            "standard_key": {"type" : "number"},
-            "priority" : {"type" : "number"},
+        "type": "object",
+        "properties": {
+            "username": {"type": "string"},
+            "standard_key": {"type": "number"},
+            "priority": {"type": "number"},
         },
     }
     validate(instance=set_standards, schema=schema)
     json_compatible_item_data = jsonable_encoder(set_standards)
     es.index(index=idx_stats, body=json.dumps(set_standards))
     # TODO: Update the priority of the selected standards, ES code to update an item to {"priority": 100}
-    #es.index(index=idx_main, body=json.dumps(selected))
+    # es.index(index=idx_main, body=json.dumps(selected))
     log_stats(request, data=set_standards)
     return JSONResponse(content=json_compatible_item_data)
 
 
 if __name__ == "__main__":
     formatter = logging.Formatter(
-    "{\"time\": \"%(asctime)s.%(msecs)03d\", \"type\": \"%(levelname)s\", \"thread\":[%(thread)d], \"msc\": %(message)s}", "%Y-%m-%d %H:%M:%S"
+        '{"time": "%(asctime)s.%(msecs)03d", "type": "%(levelname)s", "thread":[%(thread)d], "msc": %(message)s}',
+        "%Y-%m-%d %H:%M:%S",
     )
-    handler = RotatingFileHandler('log/app.log', backupCount=0)
+    handler = RotatingFileHandler("log/app.log", backupCount=0)
     logging.getLogger().setLevel(logging.DEBUG)
     fastapi_logger.addHandler(handler)
     handler.setFormatter(formatter)
@@ -253,5 +249,5 @@ if __name__ == "__main__":
     startMsg["message"] = "*** Starting Server ***"
     print(json.dumps(startMsg))
     fastapi_logger.info(json.dumps(startMsg))
-    #read_logs()
+    # read_logs()
     uvicorn.run(app, host="0.0.0.0", port=8080)
