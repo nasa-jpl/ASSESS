9d5e42bc59fe27cd69ae1d96246e46c806dbae6f
Vishal Lall <vishal.h.lall@gmail.com>
2021-04-08 21:06:49 -0400

    Add bulk_export

---
+++ api/bulk_export.py
@@ -0,0 +1,76 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
+import json
+import requests
+import uuid
+import hashlib
+import time
+
+
+def convert_to_hash(url):
+    if url and "http" in url:
+        r = requests.get(url)
+        if r.status_code == 200:
+            hash_object = hashlib.sha256(r.content)
+            result = hash_object.hexdigest()
+            return result
+    return None
+
+
+def convert_to_new(doc, client, i, new="assess_remap"):
+    if doc["datetime"]:
+        timestamp = doc["datetime"]
+    else:
+        timestamp = time.strftime("%Y/%m/%d %H:%M:%S")  # 2018-03-10 01:23:27
+
+    mappings = {
+        "_id": uuid.uuid4().hex,
+        "raw_id": doc["id"].strip("~"),
+        "doc_number": i,
+        "description": doc["description_clean"],
+        "status": doc["current_status"],
+        "technical_committee": doc["tc"],  # doc["technical_committee"]
+        "sdo": {
+            "ics": {
+                "code": doc["code"].strip("~"),
+                "field": doc["field"].strip("~"),
+                "group": doc["ground"].strip("~"),
+                "subgroup": doc["subgroup"].strip("~"),
+                "edition": doc["edition"],
+                "number_of_pages": doc["number_of_pages"],
+                "section_titles": doc["section_titles"],
+                "sections": doc["sections"],
+                "new_standard": doc["new_standard"].strip("~"),
+                "new_field": doc["new_field"].strip("~"),
+                "new_group": doc["new_group"].strip("~"),
+                "new_subgroup": doc["new_subgroup"].strip("~"),
+                "type": doc["type"],
+            }
+        },
+        "title": doc["title"].strip("~"),
+        "published_date": doc["publication_date"],
+        "isbn": None,
+        "url": doc["link"],
+        "ingestion_date": timestamp,
+        "hash": convert_to_hash(doc["link"]),
+    }
+    return
+
+
+REMOTE_URL = "https://localhost:9200/"
+LOCAL_FILE = "elasticsearch-dump.txt"
+INDEX = "iso_final_clean"
+client = Elasticsearch()
+
+# fp = open(LOCAL_FILE, "w")
+i = 0
+for doc in scan(client, query={}, index=INDEX):
+    # json.dump(row, fp)
+    # fp.write("\n")
+    # fp.flush()  # So you can tail -f the file
+    i += 1
+    print(convert_to_new(doc, client, i))
+    if i == 10:
+        exit()
+
+# fp.close()
