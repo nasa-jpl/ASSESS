71a749282e86889af0369b0f086b420bc0bc994b
Vishal Lall <vishal.h.lall@gmail.com>
2021-04-08 21:48:13 -0400

    Add bulk_export

--- api/bulk_export.py
+++ api/bulk_export.py
@@ -23,7 +23,18 @@ def strip_number(txt):
 
 
 def clean_sections(txt):
-    pass
+    """
+    ['Foreword\nISO (the International Organization for Standardization) is a worldwide federation of national standards bodies (ISO member bodies). The work of preparing International Standards is normally carried out through ISO technical committees. Each member body interested in a subject for which a technical committee has been established has the right to be represented on that committee. International organizations, governmental and nongovernmental, in liaison with ISO, also take part in the work. ISO collaborates closely with the International Electrotechnical Commission (IEC) on all matters of electrotechnical standardization.\nDraft International Standards adopted by the technical committees are circulated to the member bodies for voting. Publication as an International Standard requires approval by at least 75 % of the member bodies casting a vote.\nInternational Standard ISO 12103-2 was prepared by Technical Committee ISO/TC 22, Road vehicles, Subcommittee SC 7, Injection equipment and filters for use on road vehicles.\nISO 12103 consists of the following parts, under the general title Road vehicles — Test dust for filter evaluation :\n— Part 1: Arizona test dust\n— Part 2: Aluminium oxide test dust\nAnnexes A to C of this part of ISO 12103 are for information only.', 'Introduction\nThis part of ISO 12103 specifies a range of inorganic test dusts, manufactured from fused aluminium oxide, primarily used for evaluating the performance of both fuel and lubricating oil filters for internal combustion engines, generally by gravimetric methods.\nThis part of ISO 12103 is to be used in conjunction with a number of other International Standards which refer to the use of these test dusts in various filter performance test procedures.', '1   Scope\nThis part of ISO 12103 specifies the particle size distribution of five inorganic test dusts used for the evaluation of filters.\nThese dust are used in conjunction with various test procedures designed to evaluate, in general, fuel and lubricating oil filters for internal combustion engines by gravimetric methods.\nThe dusts may also be used for a number of other applications, such as for abrasion tests, where distinct, known particle size distributions of hard, abrasive material are required.']
+
+    """
+    res = {}
+    for section in txt:
+        try:
+            spl = section.split("\n")
+            res[spl[0]] = spl[1]
+        except Exception:
+            return None
+    return res
 
 
 def convert_to_new(doc, client, i, new="assess_remap"):
@@ -48,7 +59,7 @@ def convert_to_new(doc, client, i, new="assess_remap"):
                 "edition": strip_number(doc["edition"]),
                 "number_of_pages": strip_number(doc["number_of_pages"]),
                 "section_titles": doc["section_titles"],
-                "sections": doc["sections"],
+                "sections": clean_sections(doc["sections"]),
                 "new_standard": doc["new_standard"].strip("~"),
                 "new_field": doc["new_field"].strip("~"),
                 "new_group": doc["new_group"].strip("~"),
