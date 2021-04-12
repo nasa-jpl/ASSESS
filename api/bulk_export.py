98fb74220d1d5a88a1b2376ce6da2227da0e0267
Vishal Lall <vishal.h.lall@gmail.com>
2021-04-12 19:02:25 -0400

    Add new search functionality

--- api/bulk_export.py
+++ api/bulk_export.py
@@ -80,7 +80,7 @@ def convert_to_new(doc, client, i, new_index):
                 "preview_url": doc["preview_url"],
             }
         },
-        "text": "description",  # Change to which field is used for analysis
+        "text": ["description"],  # Change to which field is used for analysis
         "title": doc["title"].strip("~"),
         "published_date": doc["publication_date"],
         "isbn": None,
