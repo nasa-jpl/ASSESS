6d57dfc665b8824030f67fc843e562afdf93860a
Vishal Lall <vishal.h.lall@gmail.com>
2021-04-17 21:27:42 -0400

    Clean predict calls

--- api/text_analysis/extract_prep.py
+++ api/text_analysis/extract_prep.py
@@ -11,7 +11,8 @@ from standard_extractor import find_standard_ref
 import pathlib
 from elasticsearch import Elasticsearch
 from web_utils import connect_to_es
-
+from pandas.io.json import json_normalize
+from collections import deque
 
 # Connect to Elasticsearch
 es, idx_main, idx_log, idx_stats = connect_to_es()
@@ -35,13 +36,13 @@ def parse_text(filepath):
 
 
 def transfrom(df):
-    df = df[df["type"] == "standard"].reset_index(drop=True)
+    df = df[df["sdo.iso.type"] == "standard"].reset_index(drop=True)
     df.fillna("", inplace=True)
     tfidftransformer = TfidfVectorizer(
         ngram_range=(1, 1), stop_words=text.ENGLISH_STOP_WORDS
     )
     X = tfidftransformer.fit_transform(
-        [m + " " + n for m, n in zip(df["description_clean"], df["title"])]
+        [m + " " + n for m, n in zip(df["description"], df["title"])]
     )
     X = normalize(X, norm="l2", axis=1)
     nbrs_brute = NearestNeighbors(
@@ -51,7 +52,35 @@ def transfrom(df):
     return tfidftransformer, X, nbrs_brute
 
 
-def predict_from_es(file=None, text=None, size=10):
+def predict(file=None, text=None, size=10):
+    """
+    Predict recommendations given text.
+    Fields in the dataframe:
+    ['id',
+    'raw_id',
+    'doc_number',
+    'description',
+    'status',
+    'technical_committee',
+    'text',
+    'title',
+    'published_date',
+    'isbn',
+    'url',
+    'ingestion_date',
+    'hash',
+    'sdo.iso.code',
+    'sdo.iso.field',
+    'sdo.iso.group',
+    'sdo.iso.subgroup',
+    'sdo.iso.edition',
+    'sdo.iso.number_of_pages',
+    'sdo.iso.section_titles',
+    'sdo.iso.sections',
+    'sdo.iso.type',
+    'sdo.iso.preview_url',
+    'category.ics']
+    """
     if file:
         if file.filename == "":
             return "No selected file!"
@@ -64,10 +93,10 @@ def predict_from_es(file=None, text=None, size=10):
         file.write(str(new_text.encode("utf-8", "ignore")))
         file.flush()
         file.close()
-    res = es.search(index=idx_main, body={"query": {"match_all": {}}})
-    df = pd.concat(map(pd.DataFrame.from_dict, res), axis=1)["fields"].T
-    print(df)
-    exit()
+    res = list(scan(client, query={}, index=idx_main))
+    output_all = deque()
+    output_all.extend([x["_source"] for x in res])
+    df = json_normalize(output_all)
     tfidftransformer, X, nbrs_brute = transfrom(df)
 
     standard_refs = find_standard_ref(new_text)
@@ -83,146 +112,11 @@ def predict_from_es(file=None, text=None, size=10):
     indices = list(indices[0])
 
     for indx, dist in zip(indices[:size], distances[:size]):
-        title = df.iloc[indx]["title"]
-        description = df.iloc[indx]["description"]
-        link = df.iloc[indx]["link"]
-        standard_code = df.iloc[indx]["standard"]
-        standard_id = df.iloc[indx]["id"]
-        code = df.iloc[indx]["code"]
-        tc = df.iloc[indx]["tc"]
-    result["recommendations"].append(
-        {
-            "sim": 100.0 * round(1 - dist, 2),
-            "raw_id": standard_id,
-            "code": code,
-        }
-    )
-    return result
-
-
-def predict_test(file=None, in_text=None):
-    dirPath = str(pathlib.Path(__file__).parent.absolute())
-
-    standards_dir = dirPath + "/../standards/data"
-    json_output_dir = "output"
-    models_dir = "models"
-
-    # TODO: Fix this line
-    # df = pd.concat(map(pd.DataFrame.from_dict, res), axis=1)
-    df = pd.read_csv(
-        os.path.join(standards_dir, "iso_final_all_clean_text.csv"), index_col=0
-    )
-    print(df)
-    exit()
-
-
-def predict(file=None, in_text=None, size=10):
-    dirPath = str(pathlib.Path(__file__).parent.absolute())
-
-    standards_dir = dirPath + "/../standards/data"
-    json_output_dir = "output"
-    models_dir = "models"
-
-    # TODO: Fix this line
-    # df = pd.concat(map(pd.DataFrame.from_dict, res), axis=1)
-    df = pd.read_csv(
-        os.path.join(standards_dir, "iso_final_all_clean_text.csv"), index_col=0
-    )
-    # print(df2)
-    df = df[df["type"] == "standard"].reset_index(drop=True)
-    df.fillna("", inplace=True)
-
-    tfidftransformer = TfidfVectorizer(
-        ngram_range=(1, 1), stop_words=text.ENGLISH_STOP_WORDS
-    )
-    X = tfidftransformer.fit_transform(
-        [m + " " + n for m, n in zip(df["description_clean"], df["title"])]
-    )  # using both desc and tile to predict
-    # tfidftransformer = TfidfVectorizer(ngram_range=(1,1))
-    # X = tfidftransformer.fit_transform([m+' '+n for m, n in zip(df['description'], df['title'])]) # using both desc and tile to predict
-    print("shape", X.shape)
-    X = normalize(X, norm="l2", axis=1)
-    nbrs_brute = NearestNeighbors(
-        n_neighbors=X.shape[0], algorithm="brute", metric="cosine"
-    )
-    print("fitting")
-    nbrs_brute.fit(X.todense())
-    print("fitted")
-
-    # How do we get request.file
-    new_text = ""
-    # ======================== find the referenced standards
-    filename = "temp_text"
-    # check if the post request has the file part
-    if file:
-        print("made it to the PDF part")
-        # INSERT FILES OBJECT HERE
-        # file = files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == "":
-            return "no selected file!"
-            # return redirect(request.url)
-        if file:
-            filename = file.filename
-            ### Save the file
-            new_text = parse_text(filename)
-            print("parsed")
-    else:
-        # get text from form
-        new_text = in_text
-        file = open(filename, "w")
-        file.write(str(new_text.encode("utf-8", "ignore")))
-        file.flush()
-        file.close()
-
-    print("extracting standards")
-    # standard_refs=extract_standard_ref(filename)
-    standard_refs = find_standard_ref(new_text)
-    print("standards extracted")
-
-    # ======================== find the recommended standards
-
-    result = {}
-    result["embedded_references"] = standard_refs
-    result["recommendations"] = []
-
-    sow = tfidftransformer.transform([new_text])
-    sow = normalize(sow, norm="l2", axis=1)
-
-    print("scoring standards")
-    distances, indices = nbrs_brute.kneighbors(sow.todense())
-    distances = list(distances[0])
-    indices = list(indices[0])
-
-    for indx, dist in zip(indices[:size], distances[:size]):
-        title = df.iloc[indx]["title"]
-        description = df.iloc[indx]["description"]
-        link = df.iloc[indx]["link"]
-        standard_code = df.iloc[indx]["standard"]
-        standard_id = df.iloc[indx]["id"].replace("~", "")
-        code = df.iloc[indx]["code"].replace("~", "")
-        tc = df.iloc[indx]["tc"]
-        type_standard = ["Information Technology"]
-
-        # TODO: this code calculates the word importances for the top results (slows the operation, hence commented)
-        # print(title)
-        # print(description)
-        #
-        # to_print = [
-        #     tfidftransformer.get_feature_names()[i]
-        #     + ' ' +
-        #     str(abs(np.array(sow[0].todense()).flatten()[i] - np.array(X[indx].todense()).flatten()[i]))
-        #     for i in set(sow.indices).intersection(X[indx].indices)]
-        # print(' || '.join(to_print), '\n')
-
+        id = df.iloc[indx]["id"]
         result["recommendations"].append(
             {
                 "sim": 100 * round(1 - dist, 21),
-                "raw_id": standard_id,
-                "code": code,
+                "id": id,
             }
         )
-        print("Make sure it's an error here and not in ES.")
-        print(result)
-    return result
+        return result
