9162b39b52d276ee79bafd996b101122ca6bb512
vishall <vishall@mt-100340.lan>
2022-01-31 15:24:13 -0800

    Combine parsers

--- api/standards_extraction/parse.py
+++ api/standards_extraction/parse.py
@@ -2,6 +2,31 @@
 
 import re
 import io
+import os
+import subprocess
+
+
+def tika_parse(pdf):
+    filepath = "./data/" + pdf.filename
+    if os.path.exists(filepath + "_parsed.txt"):
+        # todo: remove this. Caches the parsed text.
+        return str(open(filepath + "_parsed.txt", "r").read())
+    pdf.write(filepath)
+    bashCommand = "java -jar standards_extraction/lib/tika-app-1.16.jar -t " + filepath
+    output = ""
+    try:
+        output = subprocess.check_output(["bash", "-c", bashCommand])
+        # file = open(filepath + "_parsed.txt", "wb")
+        # file.write(output)
+        # file.close()
+        # Returns bytestring with lots of tabs and spaces.
+        if type(output) == bytes:
+            output = output.decode("utf-8").replace("\t",
+                                                    " ").replace("\n", " ")
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+    return str(output)
+
 
 standard_orgs = {}
 for line in io.open("standards_extraction/data/standard_orgs.txt", mode="r", encoding="utf-8").readlines():
